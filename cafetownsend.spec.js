560f85a2d43601f7960efcea74a87eddeee743d0
deb <debashish.jagadeba@xinja.com.au>
2019-11-05 14:18:34 +1100

    added new tests

--- cafetownsend.spec.js
+++ cafetownsend.spec.js
@@ -16,40 +16,40 @@ describe('cafetownsend  App', function() {
     beforeEach(function () {
         browser.get('http://cafetownsend-angular-rails.herokuapp.com/login');
     })
-    // it('should have a title', function() {
-    //    expect(browser.getTitle()).toEqual('CafeTownsend-AngularJS-Rails');
-    // });
+    it('should have a title', function() {
+       expect(browser.getTitle()).toEqual('CafeTownsend-AngularJS-Rails');
+    });
 
-    // it('Login test',function () {
-    //     login('Luke','Skywalker');
-    //     expect(loginRes.getText()).toEqual('Hello Luke');
+    it('Login test',function () {
+        login('Luke','Skywalker');
+        expect(loginRes.getText()).toEqual('Hello Luke');
 
-    //     //expect(loginRes.getText().toEqual('Hello Luke'))
-    // });
+        //expect(loginRes.getText().toEqual('Hello Luke'))
+    });
 
-    // it('select each employee test',function () {
-    //     login('Luke','Skywalker');
-    //     expect(loginRes.getText()).toEqual('Hello Luke');
-    //     $$('#employee-list li').each(function(element, index) {
-    //         //console.log(element.getText());
-    //         // Will print 0 First, 1 Second, 2 Third.
-    //         element.getText().then(function (text) {
-    //           console.log(index, text);
+    it('select each employee test',function () {
+        login('Luke','Skywalker');
+        expect(loginRes.getText()).toEqual('Hello Luke');
+        $$('#employee-list li').each(function(element, index) {
+            //console.log(element.getText());
+            // Will print 0 First, 1 Second, 2 Third.
+            element.getText().then(function (text) {
+              console.log(index, text);
 
-    //         });
-    //       });
-    // });
+            });
+          });
+    });
 
-    // it('select and click employee test',function () {
-    //     login('Luke','Skywalker');
-    //     expect(loginRes.getText()).toEqual('Hello Luke');
-    //     $$('#employee-list li').filter(function(elem, index) {
-    //         return elem.getText().then(function(text) {
-    //           return text === 'edit123 last1';
-    //         });
-    //       }).first().click();
-    //       browser.sleep(5000);
-    // });
+    it('select and click employee test',function () {
+        login('Luke','Skywalker');
+        expect(loginRes.getText()).toEqual('Hello Luke');
+        $$('#employee-list li').filter(function(elem, index) {
+            return elem.getText().then(function(text) {
+              return text === 'edit123 last1';
+            });
+          }).first().click();
+          browser.sleep(5000);
+    });
 
     it('select and edit employee test',function () {
         login('Luke','Skywalker');
